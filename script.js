cec9f565cc917d3a6141c0bbadc3ea4bb80b9106
Sayed Ahmd <60805556+pranto78@users.noreply.github.com>
2024-01-12 20:27:01 +0600

    saesahmd0@gmail.com

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+function inputAgeNumber(){
+    let letters = "number"
+
+    let number = "inputAge";
+
+    for(let i =0; i < 18; i++){
+        number = number + letters[Math.floor(Math.number() * 18)]
+    }
+    document.getElementById('ageChild').textContent = number
+    document.ageAdult.style.display=
+}
